d67f51c3701e94dfa207290d325073c028b24f1b
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2023-09-09 04:27:05 +0530

    0.0.5

--- src/components/Page10.js
+++ src/components/Page10.js
@@ -4,71 +4,77 @@ export default function Page10() {
   return (
     <>
       <div className="Box">
-        <h1 style={{ color: "green", textAlign: "center", marginTop: "-50px" }}>
+        <h1
+          style={{
+            color: "rgb(0, 72, 114)",
+            textAlign: "center",
+            marginTop: "-50px",
+          }}
+        >
           Falcon
         </h1>
         <div className="p10Container">
           <div className="Row">
             <div className="Column">
               <h2 className="Headings">About Us</h2>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Aim
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Vision
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Testimonials
               </a>
             </div>
             <div className="Column">
               <h2 className="Headings">Services</h2>
-              <a className="FooterLink" href="#">
-                Writing
+              <a className="FooterLink" href="/">
+                Tours
               </a>
-              <a className="FooterLink" href="#">
-                Internships
+              <a className="FooterLink" href="/">
+                Hiking
               </a>
-              <a className="FooterLink" href="#">
-                Coding
+              <a className="FooterLink" href="/">
+                Tracking
               </a>
-              <a className="FooterLink" href="#">
-                Teaching
+              <a className="FooterLink" href="/">
+                Trips
               </a>
             </div>
             <div className="Column">
               <h2 className="Headings">Contact Us</h2>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Uttar Pradesh
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Ahemdabad
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Indore
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 Mumbai
               </a>
             </div>
             <div className="Column">
               <h2 className="Headings">Social Media</h2>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 <i className="fab fa-facebook-f">
                   <span style={{ marginLeft: "10px" }}>Facebook</span>
                 </i>
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 <i className="fab fa-instagram">
                   <span style={{ marginLeft: "10px" }}>Instagram</span>
                 </i>
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 <i className="fab fa-twitter">
                   <span style={{ marginLeft: "10px" }}>Twitter</span>
                 </i>
               </a>
-              <a className="FooterLink" href="#">
+              <a className="FooterLink" href="/">
                 <i className="fab fa-youtube">
                   <span style={{ marginLeft: "10px" }}>Youtube</span>
                 </i>
