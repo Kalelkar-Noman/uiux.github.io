8aa19b9a88c60f62a1e252e5762d1f925355aa78
Kalelkar_Noman <kalelkarnoman014@gmail.com>
2023-09-09 11:39:20 +0530

    Update Page1

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -18,20 +18,18 @@ export default function Page1() {
       <div className="page-1">
         <div ref={containerRef} id="container" className="container">
           <div style={{ backgroundImage: `url(${a})` }} className="slides">
-            <p> it's A Big World <br> Out There World <br> Explore</p>
+            <p> Maharashtra</p>
           </div>
-  
           <div
             style={{
               backgroundImage: `url("https://images.unsplash.com/photo-1693040162619-c3bdfaf3386a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80")`,
             }}
             className="slides"
           >
-           <p>it's A Big World <br> Out There,Go <br> Explore</p>
+            <p> Maharashtra</p>
           </div>
           <div style={{ backgroundImage: `url(${c})` }} className="slides">
-           <p> it's A Big World <br> Out There World <br> Explore</p>
-              
+            <p> Maharashtra</p>
           </div>
           <aside>
             <div
