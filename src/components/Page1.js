6bff50021c324a10b83e2f2e5fa26555e5c87b49
Kalelkar_Noman <kalelkarnoman014@gmail.com>
2023-09-09 11:17:33 +0530

    Update Page1

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -20,6 +20,7 @@ export default function Page1() {
           <div style={{ backgroundImage: `url(${a})` }} className="slides">
             <p> it's A Big World <br> Out There World <br> Explore</p>
           </div>
+  
           <div
             style={{
               backgroundImage: `url("https://images.unsplash.com/photo-1693040162619-c3bdfaf3386a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80")`,
